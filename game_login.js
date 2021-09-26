bd27e400c7359cfd6f5e902006cea62a861bba9c
dhruvmundra <81858843+dhruvmundra@users.noreply.github.com>
2021-09-26 12:17:04 +0530

    Add files via upload

---
+++ game_login.js
@@ -0,0 +1,7 @@
+function addUser(){
+    player1_name=document.getElementById("player1_input").value;
+    player2_name=document.getElementById("player2_input").value;
+
+    localStorage.setItem("player1_LS",player1_name);
+    localStorage.setItem("player2_LS",player2_name);
+}
